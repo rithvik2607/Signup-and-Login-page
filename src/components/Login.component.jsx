36960ca35ec40d85eddb42472205a4bfb29dc557
Ayithapu Sai Rithvik <rithvikayithapu@gmail.com>
2020-04-10 17:32:25 +0530

    Fixed issues with state

--- src/components/Login.component.jsx
+++ src/components/Login.component.jsx
@@ -19,7 +19,6 @@ const Login = () => {
       ...userData,
       [e.target.name]: e.target.value
     });
-    console.log(e.target.value)
   };
 
   const onSubmit = e => {
@@ -31,7 +30,9 @@ const Login = () => {
       password:''
     };
 
-    axios.post('https://git.heroku.com/login-page-backend.git', userData)
+    console.log(userData);
+
+    axios.post('http://localhost:5000/api/user/login', userData)
       .then(() => {
         setUserData(emptyLoginData);
       })
@@ -42,24 +43,30 @@ const Login = () => {
   };
 
   return(
-    <section className="body-box pb-4 mx-32">
-      <h1 className="text-center text-3xl font-bold m-8">Welcome to the login portal</h1>
-      <p className="text-center text-2xl font-medium m-8">Please enter your details to login</p>
+    <section className="body-box pb-4 md:mx-32">
+      <h1 className="text-center text-3xl font-bold md:m-8">Welcome to the login portal</h1>
+      <p className="text-center text-2xl font-medium md:m-8">Please enter your details to login</p>
       <form className="text-center mt-8 mb-8 m-48 flex flex-col" onSubmit={onSubmit}>
         <TextField
           id="standard-basic"
           label="Registration Number"
+          name="regNo"
+          value={userData.regNo}
           onChange={e => onChange(e)}
         />
         <TextField
           id="standard-basic"
           label="VIT Email"
           helperText="Please enter VIT email address"
+          name="email"
+          value={userData.email}
           onChange={e => onChange(e)}
         />
         <TextField
           id="standard-basic"
           label="Password"
+          name="password"
+          value={userData.password}
           onChange={e => onChange(e)}
         />
         <button type="submit" id="Submit" className="text-center m-8 mb-1 py-2 submit-box">Login</button>
--- src/components/SignUp.component.jsx
+++ src/components/SignUp.component.jsx
@@ -20,7 +20,7 @@ const signUp = () => {
   ]);
 
   // Declaring alert function
-  const alert = useAlert();
+  /*const alert = useAlert();*/
 
   // to handle change of input field
   const onChange = e => {
@@ -38,7 +38,7 @@ const signUp = () => {
 
   const storeData = () => {
     try{
-      axios.post('https://git.heroku.com/login-page-backend.git/signup', userData);
+      axios.post('https://git.heroku.com/login-page-backend.git/api/user/signup', userData);
     }
     catch(error){
       console.log(error);
@@ -49,7 +49,7 @@ const signUp = () => {
   const onSubmit = e => {
     e.preventDefault();
 
-    console.log(e.target.value);
+    console.log(userData);
 
     const emptyUserData = {
       firstName:'',
@@ -62,62 +62,77 @@ const signUp = () => {
       address:''
     };
 
-    storeData(userData.firstName,userData.lastName,userData.username,userData.regNo,userData.email,userData.password,userData.block,userData.address)
-    .then(() => {
-      alert.show('Profile created');
-      setUserData(emptyUserData);
-    })
-    .catch(error => {
-      alert.show('Incorrect data entered. Please check your inputs and try again.');
-      console.log(error);
-    });
+    axios.post('http://localhost:5000/api/user/signup', userData)
+      .then(() => {
+        setUserData(emptyUserData);
+      })
+      .catch(error =>{
+        /*alert.show('Incorrect data entered. Please check your inputs and try again.');*/
+        console.log(error);
+      });
   };
 
   return(
-    <section className="body-box pb-4 mx-32 my-4">
-      <h1 className="text-center text-3xl font-bold m-8">Welcome to the sign up process</h1>
-      <p className="text-center text-2xl font-medium m-8">Please enter your details to create your new profile</p>
-      <form className="text-center mt-8 mb-8 m-48 flex flex-col" onSubmit={onSubmit}>
+    <section className="body-box md:pb-4 md:mx-32 md:my-4">
+      <h1 className="text-center text-3xl font-bold md:m-8">Welcome to the sign up process</h1>
+      <p className="text-center text-2xl font-medium md:m-8">Please enter your details to create your new profile</p>
+      <form className="text-center mt-2 mb-8 m-48 flex flex-col" onSubmit={onSubmit}>
         <TextField 
           id="standard-basic" 
-          label="First Name" 
+          label="First Name"
+          name="firstName"
+          value={userData.firstName}
           onChange={e => onChange(e)} 
         />
         <TextField 
           id="standard-basic" 
-          label="Last Name" 
+          label="Last Name"
+          name="lastName"
+          value={userData.lastName} 
           onChange={e => onChange(e)} 
         />
         <TextField 
           id="standard-basic" 
           label="Username" 
+          name="username"
+          value={userData.username}
           onChange={e => onChange(e)} 
         />
         <TextField 
           id="standard-basic" 
           label="Registration Number" 
+          name="regNo"
+          value={userData.regNo}
           onChange={e => onChange(e)} 
         />
         <TextField
           id="standard-basic" 
           label="Email" 
           helperText="Please enter VIT email address" 
+          name="email"
+          value={userData.email}
           onChange={e => onChange(e)} 
         />
         <TextField 
           id="standard-basic" 
           label="Password" 
+          name="password"
+          value={userData.password}
           onChange={e => onChange(e)} 
         />
         <TextField 
           id="standard-basic" 
           label="Block" 
+          name="block"
+          value={userData.block}
           onChange={e => onChange(e)} 
         />
         <TextField multiline 
           rowsMax="4" 
           id="standard-basic" 
           label="Address"
+          name="address"
+          value={userData.address}
           onChange={e => onChange(e)} 
         />
         <button type="submit" id="Submit" className="text-center m-8 mb-1 py-2 submit-box">Sign Up</button>
